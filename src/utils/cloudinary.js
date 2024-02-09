8acc0d59aa79ac835a8444654cb66905396aa9cb
Dhananjoy Mahata <dhananjoymahata43@gmail.com>
2024-02-09 21:05:51 +0530

    Added: user controller

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,22 +1,22 @@
-import {v2 as cloudinary} from "cloudinary"
+import { v2 as cloudinary } from "cloudinary"
 import fs from "fs"
 
 
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-const uploadOnCloudinary = async (localFilePath) => {
+const uploadOnCloudinary = async (localFilePath, folder) => {
     try {
         if (!localFilePath) return null
         //upload the file on cloudinary
         const response = await cloudinary.uploader.upload(localFilePath, {
-            resource_type: "auto"
+            resource_type: "auto",
+            folder: `VideoTube/${folder}`
         })
-        // file has been uploaded successfull
-        //console.log("file is uploaded on cloudinary ", response.url);
+        // file has been uploaded successfully
         fs.unlinkSync(localFilePath)
         return response;
 
@@ -26,6 +26,31 @@ const uploadOnCloudinary = async (localFilePath) => {
     }
 }
 
+const deleteFromCloudinary = async (publicId, fileType) => {
+    try {
+        if (!publicId) return null;
+        const response = await cloudinary.api.delete_resources(publicId, { type: 'upload', resource_type: fileType });
+        if (response.deleted)
+            return true
+        else return false
 
+    } catch (error) {
+        console.error("Error deleting file from Cloudinary:", error);
+        return null;
+    }
+};
 
-export {uploadOnCloudinary}
+const extractPublicId = (cloudinaryUrl) => {
+    // Check if the provided URL is a Cloudinary URL
+    const cloudinaryPattern = /^https?:\/\/res\.cloudinary\.com\/[^/]+\/(?:video|image)\/upload\/v\d+\/(.+?)\.\w+$/;
+    const match = cloudinaryUrl.match(cloudinaryPattern);
+
+    if (match && match.length === 2) {
+        return match[1]; // Return the extracted public ID
+    } else {
+        console.error("Invalid Cloudinary URL:", cloudinaryUrl);
+        return null; // Return null if the URL doesn't match the expected pattern
+    }
+}
+
+export { uploadOnCloudinary, deleteFromCloudinary, extractPublicId }
